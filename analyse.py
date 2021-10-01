86f38bc0595e01c180f190fc5df899f841d7c1f3
gregyedlik <greg@yedlik.no>
2021-10-01 16:10:17 +0200

    Basic analysis of captured data showing ids and number of occurances.

---
+++ analyse.py
@@ -0,0 +1,32 @@
+import can
+import pickle
+import pandas
+
+captured = pickle.load(open("captured.p", "rb"))
+
+capturedFromBattery = [msg for msg in captured if msg.channel == "can0"]
+capturedFromBike = [msg for msg in captured if msg.channel == "can1"]
+
+
+def count(cap):
+    ids = [msg.arbitration_id for msg in cap]
+    unique = dict.fromkeys(ids)
+    counted = {hex(ID): ids.count(ID) for ID in unique}
+    return counted
+
+
+countedBatteryIDs = count(capturedFromBattery)
+countedBikeIDs = count(capturedFromBike)
+
+df = pandas.DataFrame(data=[countedBatteryIDs.keys(),
+                            countedBatteryIDs.values(),
+                            countedBikeIDs.keys(),
+                            countedBikeIDs.values()],
+                      index=['BatteryID', 'Count', 'BikeID', 'Count'])
+
+df = df.transpose()
+
+df.to_excel('results.xlsx')
+
+print(df)
+
