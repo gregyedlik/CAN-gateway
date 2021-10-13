5f95cbc1a85273d4237ff68b1843b101c945a51a
gregyedlik <greg@yedlik.no>
2021-10-13 17:30:06 +0200

    Automated analytics every time, renamed can0 and can1 to bike and battery.

--- analyse.py
+++ analyse.py
@@ -1,31 +1,37 @@
 import pickle
 import pandas
 
-captured = pickle.load(open("captured.p", "rb"))
 
-capturedFromBattery = [msg for msg in captured if msg.channel == "can0"]
-capturedFromBike = [msg for msg in captured if msg.channel == "can1"]
+def analyse():
+    captured = pickle.load(open("captured.p", "rb"))
+
+    first_timestamp = captured[0].timestamp
+
+    captured_important = [[hex(msg.arbitration_id), msg.channel, round(msg.timestamp - first_timestamp, 3), bytes(msg.data)] for msg in captured]
+    df_full = pandas.DataFrame(data=captured_important, columns=['arbitration ID', 'source', 'timestamp', 'data'])
+    df_full.to_excel('result_raw.xlsx')
+
+    capturedFromBattery = [msg for msg in captured if msg.channel == "battery"]
+    capturedFromBike = [msg for msg in captured if msg.channel == "bike"]
 
 
-def count(cap):
-    ids = [msg.arbitration_id for msg in cap]
-    unique = dict.fromkeys(ids)
-    counted = {hex(ID): ids.count(ID) for ID in unique}
-    return counted
+    def count(cap):
+        ids = [msg.arbitration_id for msg in cap]
+        unique = dict.fromkeys(ids)
+        counted = {hex(ID): ids.count(ID) for ID in unique}
+        return counted
 
 
-countedBatteryIDs = count(capturedFromBattery)
-countedBikeIDs = count(capturedFromBike)
+    countedBatteryIDs = count(capturedFromBattery)
+    countedBikeIDs = count(capturedFromBike)
 
-df = pandas.DataFrame(data=[countedBatteryIDs.keys(),
-                            countedBatteryIDs.values(),
-                            countedBikeIDs.keys(),
-                            countedBikeIDs.values()],
-                      index=['BatteryID', 'Count', 'BikeID', 'Count'])
+    df = pandas.DataFrame(data=[countedBatteryIDs.keys(),
+                                countedBatteryIDs.values(),
+                                countedBikeIDs.keys(),
+                                countedBikeIDs.values()],
+                          index=['BatteryID', 'Count', 'BikeID', 'Count'])
 
-df = df.transpose()
+    df = df.transpose()
 
-df.to_excel('results.xlsx')
-
-print(df)
-
+    df.to_excel('results.xlsx')
+    return
--- main.py
+++ main.py
@@ -3,45 +3,60 @@ import threading
 import pickle
 import time
 import os
+import analyse
 
 os.system('echo ncr18650b | sudo -S ./CAN_ON')
+time.sleep(1)
 
-channels = ['can0', 'can1']
-buses = [can.Bus(channel=ch,
-               interface='socketcan',  # noqa
-               bitrate=500000,  # noqa
-               recevie_own_messages=False)  # noqa
-         for ch in channels]
+channels = ['battery', 'bike']
+buses = {ch: can.ThreadSafeBus(channel=ch,
+                           interface='socketcan',  # noqa
+                           bitrate=500000,  # noqa
+                           recevie_own_messages=False)  # noqa
+         for ch in channels}
 
 print('Emptying read buffers...')
-for bus in buses:
+for bus in buses.values():
     bus.flush_tx_buffer()
-    msg = bus.recv(0.1)
-    while type(msg) == can.Message:
-        print(msg)
-        msg = bus.recv(0.1)
+    m = bus.recv(0.1)
+    while type(m) == can.Message:
+        print(m)
+        m = bus.recv(0.1)
 
 print('Read buffers empty.')
 
 captured = list()
+run = True
 
 
 def forwarder(source, destination):
     for msg in source:
-        print(msg)
+        # if msg.arbitration_id == 0x111:
+        #     msg.data[6] = 30
         destination.send(msg)
+        print(msg)
         captured.append(msg)
+        if not run:
+            break
 
 
-t1 = threading.Thread(target=forwarder, args=(buses[0], buses[1]))
-t2 = threading.Thread(target=forwarder, args=(buses[1], buses[0]))
+t1 = threading.Thread(target=forwarder, args=(buses['bike'], buses['battery']))
+t1.name = 'From bike to battery'
+t2 = threading.Thread(target=forwarder, args=(buses['battery'], buses['bike']))
+t2.name = 'From battery to bike'
 
 threads = [t1, t2]
 
 for t in threads:
     t.start()
 
-while len(captured) < 5000:
-    time.sleep(1)
+while len(captured) < 2000:
+    time.sleep(0.1)
 
 pickle.dump(captured, open("captured.p", "wb"))
+run = False
+for t in threads:
+    t.join()
+
+print('Switching off')
+analyse.analyse()
